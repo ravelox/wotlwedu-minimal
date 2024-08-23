06336e425259f806a0966d9473a9c0655a1cb796
Dave Kelly <wotlwedu@ravelox.co.uk>
2024-08-22 18:50:41 -0700

    Display all variables that are not set rather than just the first occurrence

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,17 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Check that required env variables are set
 while read var; do
-  [ -z "${!var}" ] && { echo "$var is empty or not set."; exit 1; }
-done << EOF
+  [ -z "${!var}" ] && echo "$var is unset or empty." && var_unset=true
+done << @EOF
 WOTLWEDU_API_URL
 WOTLWEDU_SERVER_NAME
 WOTLWEDU_SSL_CERT_FILE
 WOTLWEDU_SSL_KEY_FILE
-EOF
+@EOF
+    [ -n "$var_unset" ] && exit 1
 
 cd /var/opt/wotlwedu-minimal
-
 envsubst < src/app/global.ts > src/app/global.ts.tmp && mv src/app/global.ts.tmp src/app/global.ts
 ng build --configuration=production
 
